80d28cfeda1b4188efb03f0e5ae8936fe6dc4179
radiancegeorge <radiancegeorge@outlook.com>
2023-03-28 19:01:56 +0100

    updates

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const db = require("./models");
 const app = require("./src/app");
 const port = process.env.PORT;
 
-db.sequelize.sync().then(() => {
-  app.listen(port, console.log.bind(this, `listening on port ::: ${port}`));
-});
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(port, console.log.bind(this, `listening on port ::: ${port}`));
+  })
+  .catch((err) => {
+    console.error(err);
+  });
